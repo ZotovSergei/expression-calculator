fdb90d02316637bca78ff5ce9a8ec5583ca802b3
Sergei Zotov <zsv.main@gmail.com>
2020-03-11 00:09:46 +0500

    feat: add condition for sign priority

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 // }
 
 // function expressionCalculator(expr) {
- 
+
 // }
 
 // module.exports = {
@@ -13,33 +13,62 @@
 
 
 const expr = ' 35 - 45 / 37 + 84 + (  41 + 86 / 18 / 41 * 73  ) ';
-const stSign = [];
-const stNum = [];
+// const stSign = [];
+// const stNum = [];
 
 function expressionCalculator(expr) {
-    function addNumber(stNum,number) {
+    const stSign = [];
+    const stNum = [];
+    const prioritySign = {
+        '-': 1,
+        '+': 1,
+        '/': 2,
+        '*': 2,
+    }
+
+    function addNumber(stNum, number) {
 
     }
-    function addSign(params) {
-        
+
+    function addSign(stSign, sign) {
+        if (stSign.length > 0) {
+            let stSignPriority = prioritySign[stSign[stSign.length - 1]];
+            let signPriority = prioritySign[sign];
+            // console.log('----------------')
+            // console.log(stSignPriority)
+            // console.log(signPriority);
+            if (signPriority > stSignPriority) {
+                stSign.push(sign);
+            } else if (stSignPriority < signPriority) {
+                //todo: Добавить условие для действия чисел
+            }
+            // console.log(stSign.pop());
+            // console.log(stSign)
+            // debugger
+            console.log('----------------')
+
+        } else {
+            stSign.push(sign);
+        }
     }
 
 
 
     for (let index = 0; index < expr.length; index++) {
-        let number = parseFloat(expr.replace(/\D+/,''));
-        expr = expr.replace(number,'');
+        let number = parseFloat(expr.replace(/\D+/, ''));
+        expr = expr.replace(number, '');
         // let sign = expr.replace(/[-\+]/g,'');
         let sign = expr.match(/[-\+\/\(\)\*]/).join();
-        expr = expr.replace(sign,'');
-        if (number) stNum.push(number);        
-        console.log(number);
-        console.log(sign);
-        console.log(expr);
+        expr = expr.replace(sign, '');
+        if (number) stNum.push(number);
+        if (sign) addSign(stSign, sign)
+            // console.log(number);
+            // console.log(sign);
+            // console.log(expr);
         if (!sign) break;
         if (!number) break;
     }
-    console.log(stNum);
+    console.log(stSign)
 }
 
 console.log(expressionCalculator(expr));
