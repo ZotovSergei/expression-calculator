1c88f4af64a87f656269b5d78945301cb3751342
Зотов Сергей Валерьевич <__@m-invest.local>
2020-03-10 17:23:41 +0500

    feat: add parse number

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
+// function eval() {
+//     // Do not use eval!!!
+//     return;
+// }
+
+// function expressionCalculator(expr) {
+ 
+// }
+
+// module.exports = {
+//     expressionCalculator
+// }
+
+
+const expr = ' 35 - 45 / 37 + 84 + (  41 + 86 / 18 / 41 * 73  ) ';
+const stSign = [];
+const stNum = [];
 
 function expressionCalculator(expr) {
-    // write your solution here
+    function addNumber(stNum,number) {
+
+    }
+    function addSign(params) {
+        
+    }
+
+
+
+    for (let index = 0; index < expr.length; index++) {
+        let number = parseFloat(expr.replace(/\D+/,''));
+        expr = expr.replace(number,'');
+        // let sign = expr.replace(/[-\+]/g,'');
+        let sign = expr.match(/[-\+\/\(\)\*]/).join();
+        expr = expr.replace(sign,'');
+        if (number) stNum.push(number);        
+        console.log(number);
+        console.log(sign);
+        console.log(expr);
+        if (!sign) break;
+        if (!number) break;
+    }
+    console.log(stNum);
 }
 
-module.exports = {
-    expressionCalculator
-}
+console.log(expressionCalculator(expr));
