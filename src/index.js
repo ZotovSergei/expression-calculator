a7a2857bf943b705d6205a1ed93e34db6817d59a
ZotovSergei <zsv.main@gmail.com>
2020-03-21 17:49:37 +0500

    feat: realize calc

--- src/index.js
+++ src/index.js
@@ -1,126 +1,252 @@
-// function eval() {
-//     // Do not use eval!!!
-//     return;
-// }
-
-// function expressionCalculator(expr) {
-
-// }
-
-// module.exports = {
-//     expressionCalculator
-// }
-
-
-const expr = ' 35 - 45 / 37 + 84 + (  41 + 86 / 18 / 41 * 73  ) ';
-            //    1    2    1    1  0    1    2    2    2     0  
-// const stSign = [];
-// const stNum = [];
+function eval() {
+    // Do not use eval!!!
+    return;
+}
 
 function expressionCalculator(expr) {
+    let openCount = null;
+    let closeCount = null;
+    if (expr.includes('(')) {
+        let open = expr.match(/\(/g);
+        openCount = open.length;
+    }
+    if (expr.includes(')')) {
+        let close = expr.match(/\)/g);
+        closeCount = close.length;
+    }
+    if (openCount != closeCount) throw new Error('ExpressionError: Brackets must be paired');
+
+
     const stSign = [];
     const stNum = [];
+    let isOpenBracket = false;
     const prioritySign = {
         '-': 1,
         '+': 1,
         '/': 2,
         '*': 2,
     }
-    function multi(one,two) {
-        return one*two;
+    function multi(one, two) {
+        return one * two;
     }
-    function diff(one,two) {
-        return one-two;
+    function diff(one, two) {
+        return one - two;
     }
-    function addition(one,two) {
-        return one+two;
+    function addition(one, two) {
+        return one + two;
     }
-    function division(one,two) {
-        return one/two;
-    }    
-
+    function division(one, two) {
+        return one / two;
+    }
     function getLastFromStack(arr) {
         return arr.pop();
     }
-    function action(stNum, stSign) {
-        let number = 0;
-        let lastNumberStack = getLastFromStack(stNum);
-        let preLastNumberStack = getLastFromStack(stNum);
-        let act = getLastFromStack(stSign);
-        debugger
-        switch (act) {
-            case '/':
-                number = division(preLastNumberStack,lastNumberStack);                
-                break;
-            case '*':
-                number = multi(preLastNumberStack,lastNumberStack);
-                break;
-            case '+':
-                number = addition(preLastNumberStack,lastNumberStack);
-                break;
-            case '-':
-                number = diff(preLastNumberStack,lastNumberStack);
-                break;
-            default:
-                break;
-        }                
-        return number;
+    function addInArray(arr, element) {
+        if (element) arr.push(element);
     }
-
-    function addNumber(stNum, number) {
-
-    }
-
-    function addSign(stSign, sign, stNum) {
-        if (stSign.length > 0) {
-            let stSignPriority = prioritySign[stSign[stSign.length - 1]];
-            let signPriority = prioritySign[sign];
-
-            if (stSignPriority < signPriority) {
-                stSign.push(sign);                         
-            } else if (stSignPriority > signPriority || stSignPriority == signPriority) {
-                // console.log(stNum);
-                // console.log(stSign);
-                stNum.push(action(stNum, stSign));
-                console.log(stNum);
-                console.log(stSign);
-                // stSign.push(sign);
-                debugger
-                addSign(stSign,sign,stNum);
+    function innerBracketExpr(stNum, stSign) {
+        for (let index = stSign.length - 1; index >= 0; index--) {
+            if (stSign[index] == '(') {
+                stSign.pop();
+                break;
             }
-            
-            // console.log(stSign.pop());
-            // console.log(stSign)
-            // debugger
-            // console.log('----------------')
-        } else {
-            stSign.push(sign);
-            console.log('----------------')
-            console.log(stSign)
-            console.log('----------------')
+            else {
+                let element = stSign[index];
+                let stLastNumber = stNum[stNum.length - 1];
+                let stPreLastNumber = stNum[stNum.length - 2];
+                // debugger
+                switch (element) {
+                    case '/':
+                        if (stLastNumber == 0) throw new Error ("TypeError: Division by zero.");
+                        var rest = stPreLastNumber / stLastNumber;
+                        stNum.pop();
+                        stNum.pop();
+                        stSign.pop();
+                        stNum.push(rest);
+                        flag = false;
+                        // debugger                        
+                        break;
+                    case '*':
+                        var rest = stPreLastNumber * stLastNumber;
+                        stNum.pop();
+                        stNum.pop();
+                        stSign.pop();
+                        stNum.push(rest);
+                        flag = false;
+                        // debugger                        
+                        break;
+                    case '+':
+                        var rest = Number(stPreLastNumber) + Number(stLastNumber);
+                        stNum.pop();
+                        stNum.pop();
+                        stSign.pop();
+                        stNum.push(rest);
+                        flag = false;
+                        // debugger                        
+                        break;
+                    case '-':
+                        var rest = stPreLastNumber - stLastNumber;
+                        stNum.pop();
+                        stNum.pop();
+                        stSign.pop();
+                        stNum.push(rest);
+                        flag = false;
+                        // debugger                        
+                        break;
+                    case '(':
+                        // debugger
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+    function yo(stSign, stNum, element) {
+        if (stSign.length == 0) stSign.push(element);
+        else {
+            if (element == '(') {
+                stSign.push(element);
+                // debugger
+                return
+            }
+            if (element == ')') {
+                // debugger
+                innerBracketExpr(stNum, stSign);
+            }
+            else {
+                let lastElInStack = stSign[stSign.length - 1];
+                let stPriority = prioritySign[lastElInStack];
+                let curPriority = prioritySign[element];
+                if (lastElInStack == '(') {
+                    stSign.push(element);
+                    // debugger
+                } else {
+                    if (curPriority < stPriority || curPriority == stPriority) {
+                        let stLastNumber = stNum[stNum.length - 1];
+                        let stPreLastNumber = stNum[stNum.length - 2];
+                        // debugger
+                        switch (lastElInStack) {
+                            case '/':
+                                if (stLastNumber == 0) throw new Error ("TypeError: Division by zero.");
+                                var rest = stPreLastNumber / stLastNumber;
+                                stNum.pop();
+                                stNum.pop();
+                                stSign.pop();
+                                stNum.push(rest);
+                                flag = false;
+                                // debugger
+                                yo(stSign, stNum, element)
+                                break;
+                            case '*':
+                                var rest = stPreLastNumber * stLastNumber;
+                                stNum.pop();
+                                stNum.pop();
+                                stSign.pop();
+                                stNum.push(rest);
+                                flag = false;
+                                // debugger
+                                yo(stSign, stNum, element)
+                                break;
+                            case '+':
+                                var rest = Number(stPreLastNumber) + Number(stLastNumber);
+                                stNum.pop();
+                                stNum.pop();
+                                stSign.pop();
+                                stNum.push(rest);
+                                flag = false;
+                                // debugger
+                                yo(stSign, stNum, element)
+                                break;
+                            case '-':
+                                var rest = stPreLastNumber - stLastNumber;
+                                stNum.pop();
+                                stNum.pop();
+                                stSign.pop();
+                                stNum.push(rest);
+                                flag = false;
+                                // debugger
+                                yo(stSign, stNum, element)
+                                break;
+                            case '(':
+                                // debugger
+                                break;
+                            default:
+                                break;
+                        }
+                    } else if (curPriority > stPriority) {
+                        stSign.push(element);
+                    }
+                }
+            }
         }
     }
 
-
-
-    for (let index = 0; index < expr.length; index++) {
-        let number = parseFloat(expr.replace(/\D+/, ''));
-        expr = expr.replace(number, '');
-        // let sign = expr.replace(/[-\+]/g,'');
-        let sign = expr.match(/[-\+\/\(\)\*]/).join();
-        expr = expr.replace(sign, '');
-        if (number) stNum.push(number);
-        if (sign) addSign(stSign, sign, stNum)
-        // console.log(stNum);
-        // console.log(stSign);
-        // debugger
-        // console.log(number);
-        // console.log(sign);
-        // console.log(expr);
-        if (!sign) break;
-        if (!number) break;
+    let result = expr.match(/(\d+)|[\-\/\+\(\)\*]/g);
+    for (let index = 0; index < result.length; index++) {
+        const element = result[index];
+        if (parseInt(element) || element == 0) {
+            stNum.push(element);
+        }
+        else {
+            yo(stSign, stNum, element);
+        }
     }
-    // console.log(stSign)
+    debugger
+    for (let index = stSign.length - 1; index >= 0; index--) {
+        const element = stSign[index];
+        let stLastNumber = stNum[stNum.length - 1];
+        let stPreLastNumber = stNum[stNum.length - 2];
+        switch (element) {
+            case '/':
+                if (stLastNumber == 0) throw new Error ("TypeError: Division by zero.");
+                var rest = stPreLastNumber / stLastNumber;
+                stNum.pop();
+                stNum.pop();
+                stSign.pop();
+                stNum.push(rest);
+                flag = false;
+                // debugger                        
+                break;
+            case '*':
+                var rest = stPreLastNumber * stLastNumber;
+                stNum.pop();
+                stNum.pop();
+                stSign.pop();
+                stNum.push(rest);
+                flag = false;
+                // debugger                        
+                break;
+            case '+':
+                var rest = Number(stPreLastNumber) + Number(stLastNumber);
+                stNum.pop();
+                stNum.pop();
+                stSign.pop();
+                stNum.push(rest);
+                flag = false;
+                // debugger                        
+                break;
+            case '-':
+                var rest = stPreLastNumber - stLastNumber;
+                stNum.pop();
+                stNum.pop();
+                stSign.pop();
+                stNum.push(rest);
+                flag = false;
+                // debugger                        
+                break;
+            case '(':
+                // debugger
+                break;
+            default:
+                break;
+        }
+    }
+    return stNum[0];
+    // console.log(stNum[0]);
 }
 
-console.log(expressionCalculator(expr));
+module.exports = {
+    expressionCalculator
+}
